0cd9c980373bb17330f545891a14b94a7898cf33
ruszh <ruszh93@gmail.com>
2018-01-25 23:00:48 +0200

    ++

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,19 +5,13 @@
             <div class='row'>
                 <select @change="selectCoin()" class="custom-select" id="inputGroupSelect04">                
                     <option v-for='coin in coinsObject' :value="coin.rank">{{ coin.symbol }}</option>
-                </select>
-                <!-- <div class="input-group-append">
-                    <button @click='count' class="btn btn-outline-secondary" type="button">Convert</button>                
-                </div>   -->
+                </select>               
                 <div class="input-group col">         
                     <input v-model='number' @change='count' type="number" class="form-control col" aria-label="small" aria-describedby="inputGroup-sizing-sm">
                 </div>   
             </div> 
                
         </div>  
-        <!-- <div class="row">
-            selected coin: {{ selectedCoin }}
-        </div> -->
         <div class="row d-flex justify-content-center display-4" v-if='counted'>
             {{ number }} {{ selectedCoin.symbol }} = {{ price }}$
         </div>  
--- src/components/NewList.vue
+++ src/components/NewList.vue
@@ -6,9 +6,10 @@
                     <div class="row">        
                         <div class="col-1">{{index + 1}}</div> 
                         <div class="col-1">{{coin.short}}</div> 
-                        <div class="col-3">{{coin.long}}</div> 
-                        <div class="col-3">{{coin.price}}$</div> 
-                        <div class="col-3">{{Math.round(coin.mktcap)}}$</div> 
+                        <div class="col">{{coin.long}}</div> 
+                        <div class="col price">{{Math.round(coin.price * 10000) / 10000 }}$</div>
+                        <div class="col" :class="[ {up: coin.cap24hrChange > 0}, {down: coin.cap24hrChange < 0}]">{{coin.cap24hrChange}}%</div>                         
+                        <div class="col marketcap">{{Math.round(coin.mktcap).toLocaleString('ru')}}$</div> 
                     </div>
                 </li>
             </ul>  
@@ -22,17 +23,24 @@
     export default {
         data() {
             return {
-                coinsStorage: []
+                coinsStorage: [],
+                numberOfCoins: 20
             }
-        },        
-        created() {
+        }, 
+        methods: {
+            getData() {
                 fetch('http://coincap.io/front')
                     .then(res => {
                         return res.json()
                     })
                     .then(response => {                    
-                        this.coinsStorage = response;
+                        this.coinsStorage = response.slice(0, this.numberOfCoins);
                     })
+
+            }
+        },    
+        created() {
+                this.getData();
 
                 const socket = socketClient.connect('https://coincap.io');
 
@@ -41,11 +49,33 @@
                         if(coin.short === tradeMsg.coin) return tradeMsg.msg
                         return coin;
                     })                    
+                }.bind(this));
+
+                window.addEventListener('scroll', function() {
+                    if(window.pageYOffset > 200) {
+                        this.numberOfCoins = 50;
+                        this.getData()
+                    }
+                    if(window.pageYOffset < 300) {
+                        this.numberOfCoins = 20;
+                        this.getData()
+                    }
                 }.bind(this))
         }
     }
 </script>
 
 <style>
-
+    .up {
+        color: rgb(96, 161, 30)
+    }
+    .down {
+        color: red
+    }
+    .price {
+        color: rgb(185, 142, 0)
+    }
+    .marketcap {
+        color: royalblue
+    } 
 </style>
